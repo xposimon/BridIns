f2c6990d5eddc39b366f6a59ed4a2bc3c2343bac
Zhengxiang Zhou <472032034@qq.com>
2023-09-19 15:51:06 +0800

    Update BridIns.cpp
    for while switch

--- BridIns.cpp
+++ BridIns.cpp
@@ -1,29 +1,79 @@
+#include "clang/Frontend/FrontendActions.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 
-class IfStatementMatcher : public clang::ast_matchers::MatchFinder::MatchCallback {
-public:
-    virtual void run(const clang::ast_matchers::MatchFinder::MatchResult &Result) {
-        const clang::IfStmt *ifStmt = Result.Nodes.getNodeAs<clang::IfStmt>("ifStmt");
-        if (ifStmt) {
-            // Process the matched if statement
-            ifStmt->dump(); // Example: Dump the AST of the matched if statement
-        }
-    }
-};
+using namespace llvm;
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+
+static cl::extrahelp MoreHelp("\nMore help text...\n");
+
+StatementMatcher IfMatcher = ifStmt().bind("ifStmt");
+StatementMatcher ForMatcher = forStmt().bind("forStmt");
+StatementMatcher WhileMatcher = whileStmt().bind("whileStmt");
+StatementMatcher SwitchMatcher = switchStmt().bind("switchStmt");
 
 class IfStmtHandler : public MatchFinder::MatchCallback {
 public:
   virtual void run(const MatchFinder::MatchResult &Result) {
     // Access the matched 'if' statement
     const clang::IfStmt *ifStmt = Result.Nodes.getNodeAs<clang::IfStmt>("ifStmt");
-    
+    SourceLocation ifStmtLoc;
     if (ifStmt) {
       // Process the 'if' statement here
-      llvm::outs() << "Found an 'if' statement:\n";
-      ifStmt->dump();
+      ifStmtLoc = ifStmt->getBeginLoc();
+      llvm::outs() << "Found If at: " << ifStmtLoc.printToString(Result.Context->getSourceManager()) << "\n";
+      //ifStmt->dump();
+    }
+  }
+};
+
+class ForStmtHandler : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    // Access the matched 'if' statement
+    const clang::ForStmt *forStmt = Result.Nodes.getNodeAs<clang::ForStmt>("forStmt");
+    SourceLocation forStmtLoc;
+    if (forStmt) {
+      // Process the 'if' statement here
+      forStmtLoc = forStmt->getBeginLoc();
+      llvm::outs() << "Found For loop at: " << forStmtLoc.printToString(Result.Context->getSourceManager()) << "\n";
+    }
+  }
+};
+
+class WhileStmtHandler : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    // Access the matched 'if' statement
+    const clang::WhileStmt *whileStmt = Result.Nodes.getNodeAs<clang::WhileStmt>("whileStmt");
+    SourceLocation whileStmtLoc;
+    if (whileStmt) {
+      whileStmtLoc = whileStmt->getBeginLoc();
+      llvm::outs() << "Found While at: " << whileStmtLoc.printToString(Result.Context->getSourceManager()) << "\n";
+    }
+  }
+};
+
+class SwitchStmtHandler : public MatchFinder::MatchCallback {
+public:
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    // Access the matched 'if' statement
+    const clang::SwitchStmtHandler *switchStmt = Result.Nodes.getNodeAs<clang::SwitchStmtHandler>("switchStmt");
+    SourceLocation switchStmtLoc;
+    if (switchStmt) {
+      // Process the 'if' statement here
+      switchStmtLoc = switchStmt->getBeginLoc();
+      llvm::outs() << "Found If at: " << switchStmtLoc.printToString(Result.Context->getSourceManager()) << "\n";
     }
   }
 };
@@ -35,21 +85,26 @@ int main(int argc, const char **argv) {
     // Fail gracefully for unsupported options.
     llvm::errs() << ExpectedParser.takeError();
     return 1;
-  }
+    }
   CommonOptionsParser& OptionsParser = ExpectedParser.get();
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
 
   // Create a MatchFinder
   IfStmtHandler ifStmtHandler;
-  IfStatementMatcher ifStmtMatcher;
+  ForStmtHandler forStmtHandler;
+  WhileStmtHandler whileStmtHandler;
+  SwitchStmtHandler switchStmtHandler;
+
   MatchFinder Finder;
 
   // Register your matcher with the Finder
-  Finder.addMatcher(ifStmtMatcher, &ifStmtHandler);
+  Finder.addMatcher(IfMatcher, &ifStmtHandler);
+  Finder.addMatcher(ForMatcher, &forStmtHandler);
+  Finder.addMatcher(WhileMatcher, &whileStmtHandler);
+  Finder.addMatcher(SwitchMatcher, &switchStmtHandler);
 
   // Run the tool, and let the Finder find matches in the AST
   int result = Tool.run(newFrontendActionFactory(&Finder).get());
-
   return result;
 }
